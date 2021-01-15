2a338d3838a98816a85c0e02ff13d1e30cb9261b
Mario Finelli <mario@finel.li>
2021-01-14 20:08:55 -0500

    Add initial proof of concept

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use notify_rust::Notification;
+use std::{thread, time};
+
 fn main() {
-    println!("Hello, world!");
+    let twenty_minutes = time::Duration::from_secs(1200);
+
+    loop {
+        thread::sleep(twenty_minutes);
+
+        let mut notification = Notification::new()
+            .summary("20 / 20 / 20")
+            .body("Look at something 20 feet away for 20 seconds!")
+            .show()
+            .unwrap();
+
+        for i in 1..=20 {
+            thread::sleep(time::Duration::from_secs(1));
+
+            notification.body(&format!(
+                "Look at something 20 feet away for \
+                20 seconds: {}",
+                20 - i
+            ));
+            notification.update();
+        }
+    }
 }
